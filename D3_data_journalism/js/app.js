d7160f6acbca4026a1cc98c073042cb7d093234d
Rongjun Ding <68705076+Rongjun-Ding@users.noreply.github.com>
2021-01-16 11:12:46 +0800

    '3'

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -13,3 +13,17 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Select chart and append SVG area to it. Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+
+// Append a group to the SVG area translate it to the right and to the bottom
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Census Data 
+d3.csv("D3_data_journalism/data/data.csv").then(function(censusData) {
+
+    
