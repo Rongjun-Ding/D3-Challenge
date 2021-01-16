d1c1adb5741fef15fd0bd17e84464b4eee0a982d
Rongjun Ding <68705076+Rongjun-Ding@users.noreply.github.com>
2021-01-16 11:50:10 +0800

    'final

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -97,7 +97,33 @@ d3.select(this).style("stroke","#323232")
 chart.append("g")
   .call(leftAxis);
 
+// Step 6: Initialize tool tip
+  
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.state}<br>Povety Rate: ${d.poverty}<br>Health Care: ${d.healthcare}`);
+    });
 
+  // Step 7: Create tooltip in the chart
+  
+  chart.call(toolTip);
 
+  // Create axes labels
+  chart.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
+
+  chart.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
+}).catch(function(error) {
+  console.log(error);
 
 });
